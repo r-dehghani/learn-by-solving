43408a68c444d22d2162ee906f02468e6e8480a2
Anahita Zameni <Zameni.anahita@gmail.com>
2022-05-24 09:32:55 +0430

    change the solution path

--- problems/0001-FileSystem/solution/CSharp/Zanahita93/Dictionary/Program.cs
+++ problems/0001-FileSystem/solution/CSharp/Zanahita93/Dictionary/Program.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 // See https://aka.ms/new-console-template for more information
 
 //Get the input path
